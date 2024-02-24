44cbd0b2f5314955c2fafea2d99b6655146c6ee2
tolstovr <robertproducts2021@gmail.com>
2024-02-24 18:01:00 +0400

    Архитектура и навбар

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,47 @@
-import "./App.css";
+import React, { useEffect, useState } from 'react'
+import { Button } from '@mui/material'
+import './App.css'
+import Navbar from "./components/Navbar/Navbar"
+import HomePage from './pages/HomePage/HomePage'
+import { HomeRounded, SchoolRounded, LeaderboardRounded } from '@mui/icons-material'
 
-function App() {
+const App = () => {
+  const [tab, setTab] = useState("Главная")
+  const [user, setUser] = useState({
+    id: 1,
+    role: 1,
+    username: "gryazniy_abobus228",
+    password: "jopaspisey228"
+  })
+
+  /*useEffect(() => {
+    const handleUserChanged = () => {
+      setUser(fetch(null))
+    }
+  })*/
+
+  const testUser = {
+    id: 1,
+    role: 1,
+    username: "gryazniy_abobus228",
+    password: "jopaspisey228"
+  }
+
+  const handleAuth = () => {
+    alert("Never gonna give you up")
+  }
+
   return (
-    <h1>TESTIN </h1>
-  );
+    <main>
+      <Navbar current={tab} 
+              onClick={setTab} 
+              tabs={[["Главная", <HomeRounded />], ["Мои курсы", <SchoolRounded />], ["Лидерборды", <LeaderboardRounded />]]}
+              sessionUser={user}
+              handleAuth={handleAuth}/>
+      <pre>{tab}</pre>
+      { tab === "Главная" && <HomePage /> }
+    </main>
+  )
 }
 
-export default App;
+export default App
